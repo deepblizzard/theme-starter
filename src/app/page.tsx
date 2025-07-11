c724478b7fef33715afc230948cf7e0a11a2b095
Abhyudaya <pixeldrift.adv@proton.me>
2025-07-11 10:00:21 +0530

    *

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import ThemeToggleButton from '@/components/ui/theme-toggle-button';
+import React from 'react';
+
+function page() {
+  return (
+    <div>
+      <ThemeToggleButton showLabel variant="circle-blur" start="top-right" />
+    </div>
+  );
+}
+
+export default page;
