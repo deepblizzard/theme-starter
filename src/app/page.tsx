c524e173be5d349a255b4fbb9d7c68eb255acf5e
antiSymmetric <pixeldrift.adv@proton.me>
2025-08-16 22:23:23 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import ThemeToggleButton from '@/components/ui/theme-toggle-button';
 import React from 'react';
+import Link from 'next/link';
 
 function page() {
   return (
-    <div>
-      <ThemeToggleButton showLabel variant="circle-blur" start="top-right" />
+   <div className=" min-h-screen w-full h-auto max-w-96">
+      <Link href={'https://youtu.be/e0Y5wJoRhYs?si=GsKTAAWepmw_JIRT'}>
+        {'https://youtu.be/e0Y5wJoRhYs?si=GsKTAAWepmw_JIRT'}
+      </Link>
     </div>
   );
 }
 
 export default page;
+
