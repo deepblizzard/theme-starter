def71dddf1ce8fc82f14d34ce4592588c6bd8946
antiSymmetric <pixeldrift.adv@proton.me>
2025-08-17 09:48:21 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,23 @@ function page() {
       <Link href={'https://youtu.be/e0Y5wJoRhYs?si=GsKTAAWepmw_JIRT'}>
         {'https://youtu.be/e0Y5wJoRhYs?si=GsKTAAWepmw_JIRT'}
       </Link>
+     <p>mereko personally usse nafrat hai, bohot jyada <br/>
+       main usse zindagi mein vhir kabhi baat ya milna kabhi nahi chahunga itna confirm hai, uske bhaiya bolenge tab bhi nahi, sab khatam hogya abb<br/>
+       
+     parr yaha pe abb mera family ka ek aur restriction ka entry hota hai <br/>
+       kisi g ko agar main promise deta hu (aur iske case mein main galti se karr chuka hu) uske baad main 7 saal koi bhi aur g se real life mein baat ya touch (with bare hands) ekdm nahi karskta sirf pichle wale ke uske memories, honor mein ya fir jab takk yeh khud haan bolde aur mereko permission dede dusri g se baat karne ka (yeh toh hone se rha kabhi hone nahi wala isse baat karne se jyada accha 7 saal wait karlu)
+
+      <br/>
+       merese jyada usse nafrat koi nahi krta hoga itna confirm hai parr vhir bhi main sapna mein bhi nahi chahunga uske saath kuch bura ho, aur voh toh bewakoofi ka haad paar krke hai aise aise dost bnaya hai joh uske piche uske saath hi ganda ganda chiz karne ka sochte aur haste haste bolte hai<br/>
+       please bhagwan ji iss murk insaan ko safe rakhiyega isse koi galti na ho aur isse gande logo se durr rakhiyega aur hamesha isse baccha ke rakhiyega bhale hi isse nafrat hai parr yeh bhi toh ek g hi hai chahe kitna hi faltu background, mindset aur culture se hai<br/>
+       pta h youtube intentionally mereko aisa hi video recommendations feed mein daalta hai jaanta h main emotional toh hu hi <br/>
+       sirf iske memories mein hi main yeh restriction follow karrha hu, break nahi krrha 7 saal aaram se wait karlunga aakhri promise follow karrha hu iske liye<br/>
+       abb pta chla aku aapse handshake gloves mein kyu kiya tha? aur aapse irl baat bhi nahi karta aapka birthday mein bhi jyada kuch nahi kiya 
+     </p>
     </div>
   );
 }
 
 export default page;
+
 
