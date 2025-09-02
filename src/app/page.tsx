897ab79a311425fb028250f3e625b798c27da94c
antiSymmetric <pixeldrift.adv@proton.me>
2025-09-02 08:26:36 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,14 @@ import Link from 'next/link';
 function page() {
   return (
    <div className="px-2 min-h-screen w-full h-auto max-w-96">
-<p className="text-3xl font-extrabold"> TYSM!!!! thank you shreya, prisha, sana (sana roy), rishika (main 12AMWish return kardunga 🙏)</p>
+<p className="text-3xl font-extrabold"> TYSM!!!! thank you aku, iqra, shreya, prisha, sana (sana roy), rishika (main 12AMWish return kardunga 🙏)</p>
      <p className="text-xl font-semibold opacity-60">yeh akansha sahi mein ek din phele 1 sep ko 12 bje wish kii thi, aisa glti 💀🙏</p>
    </div>
   );
 }
 
 export default page;
+
 
 
 
