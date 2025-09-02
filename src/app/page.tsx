d3374cf06369eb9dfd70cfc207bdd77f59c0db37
antiSymmetric <pixeldrift.adv@proton.me>
2025-09-02 22:24:11 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,17 @@ function page() {
   return (
    <div className="px-2 min-h-screen w-full h-auto max-w-96">
 <p className="text-3xl font-extrabold"> TYSM!!!! thank you aku, iqra, shreya, prisha, sana (sana roy), rishika (main 12AMWish return kardunga 🙏)</p>
-     <p className="text-xl font-semibold opacity-60">yeh akansha sahi mein ek din phele 1 sep ko 12 bje wish kii thi, aisa glti 💀🙏</p>
-   </div>
+     <p className="text-xl font-semibold opacity-60">yeh aakansha sahi mein ek din phele 1 sep ko 12 bje wish kii thi, aisa glti 💀🙏</p>
+    <p className="text-3xl font-extrabold">{"main zarur aaplogo ko free 1 year subscription dedunga"} <Link href='https://abhyudaya.space/services' className='text-blue-600 underline'>https://abhyudaya.space/services</Link>
+  </p>
+      <p className="text-xl font-semibold opacity-60">main soccha hu various plans rakhunga various services ke liye, fully-encrypted privacy-focused, abb pta chal gya main insta pe q nhi hu? </p>
+  
+    </div>
   );
 }
 
 export default page;
+
 
 
 
