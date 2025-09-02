4d330c6447963dba59002f0b3f99a3664684ff7d
antiSymmetric <pixeldrift.adv@proton.me>
2025-09-02 08:17:48 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,22 +5,14 @@ import Link from 'next/link';
 function page() {
   return (
    <div className="px-2 min-h-screen w-full h-auto max-w-96">
-      <Link className="text-blue-600 underline bold" href={'https://youtu.be/e0Y5wJoRhYs?si=GsKTAAWepmw_JIRT'}>
-        {'https://youtu.be/e0Y5wJoRhYs?si=GsKTAAWepmw_JIRT'}
-      </Link>
-     <p>youtube pkka intentionally aisa videos mera feed mein recommend karta h, pta hoga main emotional toh hu hi, aur abhi takk guilty feel krta hu gussa mein bina soche samjhe uske naam ka ganda full form apne dosto se likhwa k copy paste kardiya, iske liye main dil se maafi bhi maang chuka hu parr vhir bhi abhi takk andar se ganda lgta h</p>
-     <p>itna confirm h meko isse nafrat h aur abb kabhi nhi milne ya baat karne wala chahe iske bhaiya bhi kyu naa bol de, sab khatam hogya h abb<br/>
-     parr vhir bhi iska yeh matlab nhi h ki isse kisi bhi tarah ganda nazar se dekhta hu <br/>
-       voh jaha p bhi h bhagwan ji please please usse safe ekdm accha se rakhiyega aur uske saare gande dost joh uske peeth piche ussi k saath ganda-ganda kaam chiz sochte h unse isko safe rakhiyega, kyunki yeh bewakoof insaan toh itne acche-acche dost hi banate h, main khud apna aankh se dekha hu flirt, forced-kiss, wedding, gangs*x aur kitna faltu chiz discuss karte h<br/>
-       please bhagwan ji iske family aur isko ko bhi saare gande insaano aur gandi chizo se durr rakhiyega bhale hi yeh kitna bhi ganda, faltu culture ki hai parr yeh bhi toh ek insaan hi hai, isme bhi toh feelings hai, please inko kabhi kuch nhi hone dijiyega aur ekdm pura accha se safe rakhiyega </p>
-     <p>mera family restriction h ki agar main kisi g ko promise dedeta hu toh vhir agla 7 saal takk msin kisi bhi aur g ko physically touch ya real life mein baat nhi krskta sirf iske tribute aur memories mein (aur iske case mein main isko promise kardiya tha), aur sabe ganda baat yeh hai ki main isse permission leke hi apna yeh restriction hata sakta hu (yeh toh hone se rha, main isse abb kabhi baat nhi krne wala 7 saal aaram se wait krlunga)</p>
-     <p>main uss din aku se handshake bhi gloves mein kiya tha, main bhale hi gussa, nafrat karta hu parr aakhri mein iske liye aakhri baar feelings aur emotions ke liye yeh restriction follow karrha hu, 7 saal takk, break nhi krrha restriction, bechari ka birthday mein bhi jaanbuch k absent rha</p>
-     <p>vhir se bhagwan ji, please gande insaano ko innlogo se durr rakhiyega aur isse mereko sacch me bhul jaaane ka strength bhi dijiega, kyunki main 7 saal baad toh pkka bhhul jaunga yeh mera zindagi mein kabhi aayi bhi thi</p>
-    </div>
+<p className="text-3xl font-extrabold"> TYSM!!!! thank you shreya, prisha, sana (sana roy), rishika (main 12AMWish return kardunga 🙏)</p>
+     <p className="text-xl font-semibold opacity-60">yeh akansha sahi mein ek din phele 1 sep ko 12 bje wish kii thi, aisa glti 💀</p>
+   </div>
   );
 }
 
 export default page;
+
 
 
 
