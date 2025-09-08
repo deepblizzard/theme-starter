4245d01f7a1433299aabb9344df58fb9d7186a0c
antiSymmetric <pixeldrift.adv@proton.me>
2025-09-08 20:56:38 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,8 +17,15 @@ function page() {
       <Link href="https://traff.co/tT3iMT2F" className="text-blue-600 underline">
         fav movies
       </Link>
+      <Link href="/" className="">
+        fav khana?? shayad koi nahi hai, vhir bhi nonveg m chicken chilli hi hai shayad aur veg mein shayad dosa
+      </Link>
+      <Link href="/" className="">
+        meri gf???? aap g log sacch m ekdm cbi, cid jaisa inquiry krte ho 💀 
+      </Link>
     </div>
   );
 }
 
 export default page;
+
