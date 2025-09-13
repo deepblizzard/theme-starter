f62ff06968b8ed94c75e5843a787166ac072bd2b
antiSymmetric <pixeldrift.adv@proton.me>
2025-09-14 00:41:10 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,21 +7,24 @@ function page() {
     <div className="px-2 min-h-screen w-full h-auto max-w-96">
       
       <Link href="/" className="">
-        dekho iqra, (politely), pls bura mtt manna parr main iss baar bhi aakansha ka hi baat ka support karrha hu <br/>
-        yeh mtt sochna voh jyada acchi dost h isiliye ya vaisa kuch, parr kyunki uska factually correct hai<br/>
-        <br/> <br/>
-        tum jab mere ekdm samne aur paas agyi (side angle) tab main kuch bhi bola tha? jabki tumhareko pata hai mere itna samne nahi aana hai <br/>
-        main tumhareko nhi hurt krne ke liye thoda sa bhi piche nhi hua <br/>
-        parr uske baad tum aage mudd gyi aur ek boy ka ek girl k just itna samne, voh bhi piche accha position nahi hai (agar tumhareko nhi pta h toh)<br/>
-        aur main maan toh rha hu tum shayad bhul gyi hogi tmhare piche main tha, (main toh tab socch hi rha tha abh yaha se hatna chahiye ya nahi), tabhi tm ulta (unintentionally) piche agyi <br/>
-        bhale hi main ghasak gya parr aku bol rhi hai ki mera aur tumhara *bad touch* hosakta aur main agree karrha hu (kyuki yeh sacch mein hosakta tha, ek baar khud sochke dekho) <br/>
-        toh pls abse (humbly) mera itna jyada paas matt aana, aur apna phone durr rkhna kabhi kabhi durr lagta hai tum mera photo kheench rhi ho 💀😅 
+        iqra ka ip addr main temporarily dusra website mein redirect krdiya hu, voh yeh nahi dekh sakti hai <br/>
+        <br/>dekho aku uska rone wala masum chehra pe mtt jaaiyega, apko pta nhi apka jaane k baad voh kya kya ki hai <br/><br/><br/>
+        mere aur apke samne toh roo rhi thi boli hum gande ladki nahi hai vahi sab, parr apka room se jaane ke baad kya hua main batata hu <br/>
+        main bed p baitha hu thi voh chair p, mera hanth bed p tha baithe huye, voh rote rote ayi aur mera hanth ke upar hi baith gyi (haan mera restriction tod di)
+        <br/> main jaldi se apna hanth hataya, thoda derr rote rote idhar udhar bolte huye, zorr se rone lagi aur mera hanth pakadh li, main apna zindagi mein phela baar kisi g ko rote huye dekh rha tha, pata nahi tha kya karu toh hanth nahi hataya (dusra baar mera restriction break)<br/>
+     vhir mera haanth ughhhhhhhhh ganda jagah k samne hanth rkh di, bass thoda hi derr baad vhir se haanth pakad li aur iss baar mere khyal se jaan buch k mera ganda jagah bhi touch krli aur main koi hard pant jeans ya cotton pant bhi nhi phena hua tha, pyjama m tha (mera restrictions ka watt lgg gya), parr voh dusra taraf dekh k roo rhi thi main soccha galti se hua parr uska baad ka activities confirm krdiya yeh most probably intentionally tha <br/>
+        main thoda piche hone ka koshish kiya <br/>
+        mera hanth zorr se pakad li, aur rone lagi (main bhi soch rha tha iska rona kab band hoga) tab achanak se mera hanth kheench li mera hanth se apna asu pochne k liye yeh toh maan bhi leta hu parr voh apna b*easts mera haanth aur elbow mein daba di, main bhi jldi se apna hanth hataya parr voh pakad k hi thi abhi bhi <br/>
+        aur voh apna dono payr faylai rote rote (mereko doubt hogya tha), aur mera ek haanth pakde huye hi mera dusra hanth bhi pakdna hai dikhate huye bed se khada huyi aur voh mera side mein baithi huyi thi aisa angle mein uthi aur mera hanth bhi kheenchi li uska sabse ganda area taraf, itna confirm tha voh sirf acting tha, voh jaan buch k touch karwane ka koshish kii thi <br/>
+        aur sacch mein ekdm sacch bol rha hu aku sab chiz itna jaldi hua mereko react krne ka time nhi mila, aur sacch mein maaf karna please please mera hanth se voh press kar li apna ganda area, main uska dusra hanth taraf dekh rha tha joh mera dusra hanth taraf jaa rha tha, mereko phele laga uska thigh ya vaisa kuch touch hua hoga, yeh bilkul bhi main nahi soccha tha, iss taraf dekhte hi jaldi se apna hanth kheencha aur uska hanth hataya<br/>
+        
       </Link>
     </div>
   );
 }
 
 export default page;
+
 
 
 
