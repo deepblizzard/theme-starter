4a8c9d4d6db09e52f83b33f5ed84ea9f9f3c4234
antiSymmetric <pixeldrift.adv@proton.me>
2025-09-14 00:47:51 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,16 +14,19 @@ function page() {
         <br/> main jaldi se apna hanth hataya, thoda derr rote rote idhar udhar bolte huye, zorr se rone lagi aur mera hanth pakadh li, main apna zindagi mein phela baar kisi g ko rote huye dekh rha tha, pata nahi tha kya karu toh hanth nahi hataya (dusra baar mera restriction break)<br/>
      vhir mera haanth ughhhhhhhhh ganda jagah k samne hanth rkh di, bass thoda hi derr baad vhir se haanth pakad li aur iss baar mere khyal se jaan buch k mera ganda jagah bhi touch krli aur main koi hard pant jeans ya cotton pant bhi nhi phena hua tha, pyjama m tha (mera restrictions ka watt lgg gya), parr voh dusra taraf dekh k roo rhi thi main soccha galti se hua parr uska baad ka activities confirm krdiya yeh most probably intentionally tha <br/>
         main thoda piche hone ka koshish kiya <br/>
-        mera hanth zorr se pakad li, aur rone lagi (main bhi soch rha tha iska rona kab band hoga) tab achanak se mera hanth kheench li mera hanth se apna asu pochne k liye yeh toh maan bhi leta hu parr voh apna b*easts mera haanth aur elbow mein daba di, main bhi jldi se apna hanth hataya parr voh pakad k hi thi abhi bhi <br/>
+        mera hanth zorr se pakad li, aur rone lagi (main bhi soch rha tha iska rona kab band hoga) tab achanak se mera hanth kheench li mera hanth se apna asu pochne k liye yeh toh maan bhi leta hu parr voh apna b*easts mera haanth aur elbow mein daba di, main bhi jldi se apna hanth hataya parr voh pakad k hi thi abhi bhi (aur main pagal log jaisa pata nahi kyu abhi bhi isko accidentally hi samjha)<br/>
         aur voh apna dono payr faylai rote rote (mereko doubt hogya tha), aur mera ek haanth pakde huye hi mera dusra hanth bhi pakdna hai dikhate huye bed se khada huyi aur voh mera side mein baithi huyi thi aisa angle mein uthi aur mera hanth bhi kheenchi li uska sabse ganda area taraf, itna confirm tha voh sirf acting tha, voh jaan buch k touch karwane ka koshish kii thi <br/>
         aur sacch mein ekdm sacch bol rha hu aku sab chiz itna jaldi hua mereko react krne ka time nhi mila, aur sacch mein maaf karna please please mera hanth se voh press kar li apna ganda area, main uska dusra hanth taraf dekh rha tha joh mera dusra hanth taraf jaa rha tha, mereko phele laga uska thigh ya vaisa kuch touch hua hoga, yeh bilkul bhi main nahi soccha tha, iss taraf dekhte hi jaldi se apna hanth kheencha aur uska hanth hataya<br/>
-        
+        uske baad voh rote rote kya boli kuch yaad nhi h mera sirf yhi socch rha tha yeh insaan mera restrictions ka dhajjiya uda di, main isse abb kbhi baat nhi krne wala) thoda derr baad tum ayi lays aur pepsi leke aur yeh aise rone ka acting karne lagi jaise kuch hua hi nahi hai <br/>
+        ussd toh baat karne ka mann nahi tha parr gussa mein galti se tu
+      aap pe hi chilla diya maaf kijiyega,mera mental state thik nhi tha uss time<br/>
       </Link>
     </div>
   );
 }
 
 export default page;
+
 
 
 
